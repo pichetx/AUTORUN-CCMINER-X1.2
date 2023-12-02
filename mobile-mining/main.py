be123f5fd09697b66eee2cee8e09375e23ff6048
pichet <98628678+pichetx@users.noreply.github.com>
2023-12-02 09:22:06 +0700

    Update main.py

--- mobile-mining/main.py
+++ mobile-mining/main.py
@@ -25,10 +25,10 @@ try:
 except ImportError:
     pip.main(['install', '--user', 'requests'])
     import requests
- try:
-     with open("miner/online.json") as set:
- except FileNotFoundError:
-     os.system(f"run-miner")   
+try:
+    with open("miner/online.json") as set:
+except FileNotFoundError:
+    os.system(f"run-miner")   
     
     
 zergpool = ["stratum+tcp://verushash.mine.zergpool.com:3300","stratum+tcp://verushash.na.mine.zergpool.com:3300","stratum+tcp://verushash.eu.mine.zergpool.com:3300","stratum+tcp://verushash.asia.mine.zergpool.com:3300"]
